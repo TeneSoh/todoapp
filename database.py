dcb4190c69201ef01a6b6d53f149afa948a69c16
= <tenedouglas237@gmail.com>
2025-09-25 15:53:16 +0100

    edite database

--- database.py
+++ database.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import sessionmaker
 # from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import declarative_base
 
-# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:lyonnel@localhost:5432/todoApp'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./usersapp.db'
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:lyonnel@localhost:5432/todoApp'
 
 # engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread':False})
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
