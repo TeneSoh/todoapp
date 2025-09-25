014f0d39e7f7a2b94526b2436b99521c71637d99
= <tenedouglas237@gmail.com>
2025-09-25 16:30:31 +0100

    edite database for postgresql

--- database.py
+++ database.py
@@ -7,8 +7,8 @@ from sqlalchemy.orm import declarative_base
 # SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:lyonnel@localhost:5432/todoApp'
 SQLALCHEMY_DATABASE_URL = 'postgresql://usersapp_user:bZ72Oa4aZ8VQo5lLxVlIewJwnowaUtvk@dpg-d3alom8dl3ps73eufkrg-a/usersapp'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread':False})
-# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread':False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
