56c383d4d713c55a7c08ff5b096e946251a67f4d
= <tenedouglas237@gmail.com>
2025-09-25 16:25:01 +0100

    edite database for postgresql

--- database.py
+++ database.py
@@ -3,8 +3,9 @@ from sqlalchemy.orm import sessionmaker
 # from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./usersapp.db'
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./usersapp.db'
 # SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:lyonnel@localhost:5432/todoApp'
+SQLALCHEMY_DATABASE_URL = 'postgresql://usersapp_user:bZ72Oa4aZ8VQo5lLxVlIewJwnowaUtvk@dpg-d3alom8dl3ps73eufkrg-a/usersapp'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread':False})
 # engine = create_engine(SQLALCHEMY_DATABASE_URL)
